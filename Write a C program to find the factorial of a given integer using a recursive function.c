718bebd8a7c2922f6a72036c889aba54dbb27962
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-18 20:42:20 +0530

    Create Write a C program to find the factorial of a given integer using a recursive function.c

---
+++ Write a C program to find the factorial of a given integer using a recursive function.c
@@ -0,0 +1,28 @@
+Write a C program to find the factorial of a given integer using a recursive function.
+
+#include <stdio.h>
+
+// Function to find the factorial of a number using recursion
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // Check for negative input
+    if (num < 0) {
+        printf("Error: Factorial is not defined for negative integers.\n");
+    } else {
+        int result = factorial(num);
+        printf("Factorial of %d is %d.\n", num, result);
+    }
+
+    return 0;
+}
