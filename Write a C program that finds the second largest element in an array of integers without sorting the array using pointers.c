e8df31cd53feeff8c2596d5f477eace2274a761f
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-18 20:47:28 +0530

    Create Write a C program that finds the second largest element in an array of integers without sorting the array using pointers.c

---
+++ Write a C program that finds the second largest element in an array of integers without sorting the array using pointers.c
@@ -0,0 +1,30 @@
+//Write a C program that finds the second largest element in an array of integers without sorting the array using pointers.
+
+#include <stdio.h>
+
+int findSecondLargest(int arr[], int size) {
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] < largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return secondLargest;
+}
+
+int main() {
+    int arr[] = { 12, 35, 1, 10, 34, 1 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int secondLargest = findSecondLargest(arr, size);
+
+    printf("The second largest element in the array is: %d\n", secondLargest);
+
+    return 0;
+}
