3b7799b503467e7b5b9553346fee5b02c87c8b70
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-19 19:26:25 +0530

    Update Write a program that takes two numbers as input from the user and swaps their values without using a temporary variable.c

--- Write a program that takes two numbers as input from the user and swaps their values without using a temporary variable.c
+++ Write a program that takes two numbers as input from the user and swaps their values without using a temporary variable.c
@@ -1,58 +1,42 @@
 //Write a program that takes two numbers as input from the user and swaps their values without using a temporary variable.
 
 //method 1
-#include <stdio.h>
 
+#include <stdio.h>
 void swap(int *a, int *b) {
-    // Performing bitwise XOR to swap values without using a temporary variable
+    //xor method
     *a = *a ^ *b;
     *b = *a ^ *b;
     *a = *a ^ *b;
 }
-
 int main() {
     int num1, num2;
-
-    // Taking input from user
     printf("Enter two numbers: ");
     scanf("%d%d", &num1, &num2);
-
     printf("Before swap: num1 = %d, num2 = %d\n", num1, num2);
-
-    // Swapping values using the swap() function
     swap(&num1, &num2);
-
     printf("After swap: num1 = %d, num2 = %d\n", num1, num2);
-
     return 0;
 }
 
 
 //method 2
+
 #include <stdio.h>
-
 void swapWithoutTemp(int *a, int *b) {
-    *a = *a + *b;  // Adding both numbers and storing the sum in the first number
-    *b = *a - *b;  // Subtracting the original second number from the sum to get the original first number
-    *a = *a - *b;  // Subtracting the original first number from the sum to get the original second number
+    *a = *a + *b;  
+    *b = *a - *b;  
+    *a = *a - *b;  
 }
-
 int main() {
     int num1, num2;
-
-    // Input two numbers from the user
     printf("Enter first number: ");
     scanf("%d", &num1);
     printf("Enter second number: ");
     scanf("%d", &num2);
-
-    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
-
-    // Call the function to swap the numbers without a temporary variable
+    printf("Before swapp: num1 = %d, num2 = %d\n", num1, num2);
     swapWithoutTemp(&num1, &num2);
-
-    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
-
+    printf("After swap: num1 = %d, num2 = %d\n", num1, num2);
     return 0;
 }
 
