36684c44f4b72d3940f26527994f60ef1ae5d9c1
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-19 19:28:53 +0530

    Update Write a c program that takes two matrices as input from the user and performs matrix multiplication.c

--- Write a c program that takes two matrices as input from the user and performs matrix multiplication.c
+++ Write a c program that takes two matrices as input from the user and performs matrix multiplication.c
@@ -1,8 +1,6 @@
 //Write a c program that takes two matrices as input from the user and performs matrix multiplication.
 
 #include <stdio.h>
-
-// Function to read matrix from user
 void readMatrix(int matrix[10][10], int rows, int cols) {
     printf("Enter the elements of the matrix:\n");
     for (int i = 0; i < rows; i++) {
@@ -12,8 +10,6 @@ void readMatrix(int matrix[10][10], int rows, int cols) {
         }
     }
 }
-
-// Function to display matrix
 void displayMatrix(int matrix[10][10], int rows, int cols) {
     printf("Matrix:\n");
     for (int i = 0; i < rows; i++) {
@@ -23,8 +19,6 @@ void displayMatrix(int matrix[10][10], int rows, int cols) {
         printf("\n");
     }
 }
-
-// Function to perform matrix multiplication
 void multiplyMatrix(int matrix1[10][10], int matrix2[10][10], int result[10][10], int rows1, int cols1, int rows2, int cols2) {
     for (int i = 0; i < rows1; i++) {
         for (int j = 0; j < cols2; j++) {
@@ -35,47 +29,28 @@ void multiplyMatrix(int matrix1[10][10], int matrix2[10][10], int result[10][10]
         }
     }
 }
-
 int main() {
     int matrix1[10][10], matrix2[10][10], result[10][10];
     int rows1, cols1, rows2, cols2;
-
-    // Input number of rows and columns for the first matrix
+    // first matrix
     printf("Enter number of rows for matrix1: ");
     scanf("%d", &rows1);
     printf("Enter number of columns for matrix1: ");
     scanf("%d", &cols1);
-
-    // Input number of rows and columns for the second matrix
+    //second matrix
     printf("Enter number of rows for matrix2: ");
     scanf("%d", &rows2);
     printf("Enter number of columns for matrix2: ");
     scanf("%d", &cols2);
-
-    // Check if matrix multiplication is possible
+    //matrix multiplication condition
     if (cols1 != rows2) {
         printf("Matrix multiplication is not possible. Number of columns in matrix1 must be equal to the number of rows in matrix2.\n");
         return 0;
     }
-
-    // Read first matrix from user
     readMatrix(matrix1, rows1, cols1);
-
-    // Read second matrix from user
     readMatrix(matrix2, rows2, cols2);
-
-    // Display original matrices
-    printf("Matrix1:\n");
-    displayMatrix(matrix1, rows1, cols1);
-    printf("Matrix2:\n");
-    displayMatrix(matrix2, rows2, cols2);
-
-    // Perform matrix multiplication
     multiplyMatrix(matrix1, matrix2, result, rows1, cols1, rows2, cols2);
-
-    // Display result matrix
     printf("Result Matrix:\n");
     displayMatrix(result, rows1, cols2);
-
     return 0;
 }
