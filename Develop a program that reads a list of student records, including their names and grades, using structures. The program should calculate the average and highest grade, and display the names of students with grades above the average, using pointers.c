931bcacfb337fb3daac4d6277a83c45645d425a1
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-19 18:34:33 +0530

    Update Develop a program that reads a list of student records, including their names and grades, using structures. The program should calculate the average and highest grade, and display the names of students with grades above the average, using pointers.c

--- Develop a program that reads a list of student records, including their names and grades, using structures. The program should calculate the average and highest grade, and display the names of students with grades above the average, using pointers.c
+++ Develop a program that reads a list of student records, including their names and grades, using structures. The program should calculate the average and highest grade, and display the names of students with grades above the average, using pointers.c
@@ -4,53 +4,49 @@
 #include <string.h>
 
 // Structure to store student record
-struct Student {
+struct stud {
     char name[50];
     int grade;
 };
 
 int main() {
-    int numStudents;
+    int numstud;
     printf("Enter the number of students: ");
-    scanf("%d", &numStudents);
+    scanf("%d", &numstud);
 
-    // Dynamically allocate memory for an array of students
-    struct Student* students = (struct Student*) malloc(numStudents * sizeof(struct Student));
+    struct stud* students = (struct stud*) malloc(numstud * sizeof(struct stud));
 
-    // Read student records from user
-    for (int i = 0; i < numStudents; i++) {
+    // Reading student records from the user
+    for (int i = 0; i < numstud; i++) {
         printf("Enter the name of student %d: ", i + 1);
         scanf("%s", students[i].name);
         printf("Enter the grade of student %d: ", i + 1);
         scanf("%d", &students[i].grade);
     }
-
-    // Calculate average grade
+    // average 
     int sum = 0;
-    for (int i = 0; i < numStudents; i++) {
+    for (int i = 0; i < numstud; i++) {
         sum += students[i].grade;
     }
-    double average = (double)sum / numStudents;
+    double average = (double)sum / numstud;
 
-    // Find highest grade
+    //highest_grade
     int highest = students[0].grade;
-    for (int i = 1; i < numStudents; i++) {
+    for (int i = 1; i < numstud; i++) {
         if (students[i].grade > highest) {
             highest = students[i].grade;
         }
     }
-
     printf("\nAverage Grade: %.2lf\n", average);
     printf("Highest Grade: %d\n", highest);
     printf("Students with grades above average:\n");
-    for (int i = 0; i < numStudents; i++) {
+    for (int i = 0; i < numstud; i++) {
         if (students[i].grade > average) {
             printf("%s\n", students[i].name);
         }
     }
-
-    // Free dynamically allocated memory
     free(students);
 
     return 0;
 }
+
